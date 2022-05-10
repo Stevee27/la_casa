8b6d5bb25485077d391d005ce4d2627e0e33e451
Stephen Messina <steve@Stephens-MacBook-Pro.local>
2022-05-09 21:13:07 -0400

    preparing for cart display

--- lib/cart/cart_page.dart
+++ lib/cart/cart_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:la_casa/utils/widgets/loading_view.dart';
 
 import '../nav/nav_bar.dart';
 import 'bloc/cart_bloc.dart';
@@ -28,10 +29,16 @@ class _CartPageState extends State<CartPage> {
                     fit: BoxFit.fitWidth))),
         backgroundColor: Colors.white70,
       ),
-      body: BlocBuilder<CartCubit, CartState>(
-          // bloc: BlocProvider.of<CartCubit>(context),
-          builder: (context, state) {
-        return const Center(child: Text('You have no items in cart.'));
+      body: BlocBuilder<CartCubit, CartState>(builder: (context, state) {
+        if (state.status == CartStatus.success) {
+          if (state.items.isNotEmpty) {
+            return Center(child: Text('You have ${state.items.length.toString()} items in cart.'));
+          } else {
+            return const Center(child: Text('You have no items in cart.'));
+          }
+        } else {
+          return const LoadingView();
+        }
       }),
       bottomNavigationBar: const NavBar(2),
     );
